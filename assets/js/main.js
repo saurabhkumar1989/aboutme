d68fa8524fe94cadbfb4a46da6f70df91ae7bcf8
saurabhkumar1989 <saurabhkumar.spsu@gmail.com>
2017-04-17 11:03:46 -0400

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,38 @@
 jQuery(document).ready(function($) {
 
-
+  var show_less = "Show Less"
+  var show_more = "More"
+  $('#btn1').text(show_less);
+  $('#btn2').text(show_less);
+  $('#btn1').click(function(){
+    $(this).text(function(i,old){
+        return old==show_less ?  show_more : show_less;
+    });
+});
+  $('#btn2').click(function(){
+    $(this).text(function(i,old){
+        return old==show_less ?  show_more : show_less;
+    });
+});
+    
     /*======= Skillset *=======*/
     
     
-    $('.level-bar-inner').css('width', '0');
+    // $('.level-bar-inner').css('width', '0');
     
-    $(window).on('load', function() {
+    // $(window).on('load', function() {
 
-        $('.level-bar-inner').each(function() {
+    //     $('.level-bar-inner').each(function() {
         
-            var itemWidth = $(this).data('level');
+    //         var itemWidth = $(this).data('level');
             
-            $(this).animate({
-                width: itemWidth
-            }, 800);
+    //         $(this).animate({
+    //             width: itemWidth
+    //         }, 800);
             
-        });
+    //     });
 
-    });
+    // });
    
     
 
